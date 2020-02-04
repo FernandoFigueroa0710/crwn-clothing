98a833bb5a8e240c4d42cc332f7851c497a98f1a
Fernando Adan Figueroa Abarca <40035347+FernandoFigueroa0710@users.noreply.github.com>
2020-02-04 14:23:21 -0600

    Sign in form
    signin form component completed and styled. Needs review on input events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header.component";
+import SignInAndSignupPage from "./components/pages/loginRegister/signIn-signUp-page.component";
 import HomePage from "./components/pages/homepage/homePage.component";
 import ShopPage from "./components/pages/shop/shop.component";
 
@@ -11,6 +12,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignupPage} />
       </Switch>
     </div>
   );
