8e2e047d257fb9b78b01f45f915b23bb0d44c2fb
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-02-10 10:50:16 -0600

    cart dropdown component and cart icon toggle dropdown on and off with Redux state

--- src/components/cart/cart-icon.component.jsx
+++ src/components/cart/cart-icon.component.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping_bag.svg";
 
-const CartIcon = () => (
-	<div className="cart-icon">
+import { connect } from "react-redux";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+
+const CartIcon = ({ toggleCartHidden }) => (
+	<div className="cart-icon" onClick={toggleCartHidden}>
 		<ShoppingIcon className="shopping-icon" />
 		<span className="item-count">0</span>
 	</div>
 );
-export default CartIcon;
+
+const mapDispatchToProps = dispatch => ({
+	toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+export default connect(null, mapDispatchToProps)(CartIcon);
