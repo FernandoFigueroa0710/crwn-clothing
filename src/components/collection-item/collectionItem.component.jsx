1b79b0fc8a198c71a9c462328a80693bed1c78d6
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-02-10 11:17:10 -0600

    product cards added buttons and custom styles

--- src/components/collection-item/collectionItem.component.jsx
+++ src/components/collection-item/collectionItem.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import CustomButton from "../utils/custom-button.component";
 
 const CollectionItem = ({ id, name, imageUrl, price }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
-    </div>
-  </div>
+	<div className="collection-item">
+		<div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+		<div className="collection-footer">
+			<span className="name">{name}</span>
+			<span className="price">{price}</span>
+		</div>
+		<CustomButton inverted>ADD TO CART</CustomButton>
+	</div>
 );
 export default CollectionItem;
