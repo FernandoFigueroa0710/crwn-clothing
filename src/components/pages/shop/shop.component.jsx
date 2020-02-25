6dc7bb5967ee674ff365e8e6db71fd2825017a03
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-02-25 14:30:01 -0600

    Route compunent added to the Shop component

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Route} from "react-router-dom";
 import CollectionsOverview from "../../collections-overview/collections-overview.component";
 
-const ShopPage = () => (
+const ShopPage = ({match}) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
   </div>
 );
 
