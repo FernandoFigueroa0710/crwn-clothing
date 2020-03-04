52582ae4e0a33f486339f1be7a89994157a27c25
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-04 09:42:52 -0600

    Collection compoent Mapping all the info and rendering components

--- src/components/pages/categories/category.component.jsx
+++ src/components/pages/categories/category.component.jsx
@@ -4,10 +4,15 @@ import { selectCollection } from "../../../redux/shop/shop.selector";
 import CollectionItem from "../../collection-item/collectionItem.component";
 
 const CollectionPage = ({ collection }) => {
-  console.log("Here", collection);
+  const { title, items } = collection;
   return (
-    <div className="category">
-      <h2>Category page</h2>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
