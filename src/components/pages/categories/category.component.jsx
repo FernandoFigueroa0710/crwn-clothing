0e0ebaf5b27d4ced2599613ec98fe683e1c830db
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-04 09:11:11 -0600

    Collection Functions Selector functions and redux connect in collection page set

--- src/components/pages/categories/category.component.jsx
+++ src/components/pages/categories/category.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../../redux/shop/shop.selector";
+import CollectionItem from "../../collection-item/collectionItem.component";
 
-//import CollectionItem from "../../collection-item/collectionItem.component";
-
-const CollectionPage = ({ match }) => {
-  console.log("MATCH", match);
+const CollectionPage = ({ collection }) => {
+  console.log("Here", collection);
   return (
     <div className="category">
       <h2>Category page</h2>
     </div>
   );
 };
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
-export default CollectionPage;
+export default connect(mapStateToProps)(CollectionPage);
