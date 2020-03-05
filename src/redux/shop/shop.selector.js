5cf085d4660db802d5f9daa4e9f0420821b2b860
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-05 11:13:10 -0600

    SHOP SELECTOR Data optimization for the shop selector component

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
-
 const selectShop = state => state.shop;
 
 export const selectCollectionsItems = createSelector(
@@ -16,9 +8,8 @@ export const selectCollectionsItems = createSelector(
 );
 
 export const selectCollection = collectionUrlParam => {
-  return createSelector([selectCollectionsItems], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  return createSelector(
+    [selectCollectionsItems],
+    collections => collections[collectionUrlParam]
   );
 };
