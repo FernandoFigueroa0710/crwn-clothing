95c1537b986a99dab3b81b390ed40a0f3a2dbe20
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-29 19:28:43 -0600

    CUSTOM BUTTON: Added styles component to button

--- src/components/collection-item/collectionItem.component.jsx
+++ src/components/collection-item/collectionItem.component.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
-import CustomButton from "../utils/custom-button.component";
+import CustomButton from "../custom-button/custom-button.component";
 import { addItem } from "../../redux/cart/cart.actions";
 
 const CollectionItem = ({ item, addItem }) => {
 	const { imageUrl, name, price } = item;
 	return (
 		<div className="collection-item">
-			<div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+			<div
+				className="image"
+				style={{ backgroundImage: `url(${imageUrl})` }}
+			/>
 			<div className="collection-footer">
 				<span className="name">{name}</span>
 				<span className="price">{price}</span>
--- src/components/pages/loginRegister/signIn.component.jsx
+++ src/components/pages/loginRegister/signIn.component.jsx
@@ -1,63 +1,63 @@
 import React, { Component } from "react";
 import FormInput from "../../utils/form-input.component";
-import CustomButton from "../../utils/custom-button.component";
+import CustomButton from "../../custom-button/custom-button.component";
 
 import { signInWithGoogle } from "../../utils/firebase.utils";
 class SignIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			password: ""
+		};
+	}
 
-  handleSubmit = event => {
-    event.preventDefault();
+	handleSubmit = event => {
+		event.preventDefault();
 
-    this.setState({ email: "", password: "" });
-  };
+		this.setState({ email: "", password: "" });
+	};
 
-  handleChange = event => {
-    event.preventDefault();
-    const { value, name } = event.target;
-    this.setState({ [name]: value });
-  };
-  render() {
-    return (
-      <div className="sign-in">
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
+	handleChange = event => {
+		event.preventDefault();
+		const { value, name } = event.target;
+		this.setState({ [name]: value });
+	};
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I already have an account</h2>
+				<span>Sign in with your email and password</span>
 
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name="Email"
-            type="email"
-            value={this.state.email}
-            handleChange={this.handleChange}
-            label="Email"
-            required
-          />
-          <FormInput
-            name="Password"
-            type="password"
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label="Password"
-            required
-          />
-          <div className="buttons">
-            <CustomButton type="submit" value="Submit form">
-              Sign in
-            </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign in with Google
-            </CustomButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						name="Email"
+						type="email"
+						value={this.state.email}
+						handleChange={this.handleChange}
+						label="Email"
+						required
+					/>
+					<FormInput
+						name="Password"
+						type="password"
+						value={this.state.password}
+						handleChange={this.handleChange}
+						label="Password"
+						required
+					/>
+					<div className="buttons">
+						<CustomButton type="submit" value="Submit form">
+							Sign in
+						</CustomButton>
+						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+							Sign in with Google
+						</CustomButton>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignIn;
