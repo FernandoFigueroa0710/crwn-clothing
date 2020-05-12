acf2da76821ceca674a6d41a0add50bf2596da7b
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2020-05-12 13:44:32 -0500

    Cart Dripdown updated styles

--- src/components/cart/cart-icon.component.jsx
+++ src/components/cart/cart-icon.component.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping_bag.svg";
-
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
 import { selectCartItemsCount } from "../../redux/cart/cart.selector";
+import {
+  CartContainer,
+  ShoppingIcon,
+  ItemCountContainer
+} from './cart-icon.styles';
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{itemCount}</span>
-  </div>
+  <CartContainer onClick={toggleCartHidden}>
+    <ShoppingIcon />
+    <ItemCountContainer>{itemCount}</ItemCountContainer>
+  </CartContainer>
 );
 
 const mapDispatchToProps = dispatch => ({
