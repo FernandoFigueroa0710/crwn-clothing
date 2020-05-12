d97bed1108659f86986c4ce2bd1eaac07ffb887d
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2020-05-12 10:15:48 -0500

    Collections overview container styled

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import {CollectionsOverviewContainer} from "./collections-overview.styles";
 import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
 
 import CollectionPreview from "../../components/preview-collection/previewCollection.component";
 
 const CollectionsOverview = ({ collections }) => (
-	<div className="collections-overview">
-		{collections.map(({ id, ...otherCollectionProps }) => (
+	<CollectionsOverviewContainer>
+			{collections.map(({ id, ...otherCollectionProps }) => (
 			<CollectionPreview key={id} {...otherCollectionProps} />
 		))}
-	</div>
+	</CollectionsOverviewContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
