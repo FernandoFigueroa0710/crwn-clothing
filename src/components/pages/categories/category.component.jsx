60539e6c7ebeb1724e836216c7b4dec2e55e567f
FernandoFigueroa0710 <fernandoffigueroa@gmail.com>
2020-05-27 18:18:21 -0500

    Categories Page Styled Comp

--- src/components/pages/categories/category.component.jsx
+++ src/components/pages/categories/category.component.jsx
@@ -2,24 +2,29 @@ import React from "react";
 import { connect } from "react-redux";
 import { selectCollection } from "../../../redux/shop/shop.selector";
 import CollectionItem from "../../collection-item/collectionItem.component";
+import {
+  CategoryPageContainer,
+  CategoryTitle,
+  CategoryItemsContainer,
+} from "./categoryPageStyles";
 
 const CollectionPage = ({ collection }) => {
-	const { title, items } = collection;
-	return (
-		<div className="collection-page">
-			<h2 className="title">{title}</h2>
-			<div className="items">
-				{items.map(item => (
-					<CollectionItem key={item.id} item={item} />
-				))}
-			</div>
-		</div>
-	);
+  const { title, items } = collection;
+  return (
+    <CategoryPageContainer>
+      <CategoryTitle className="title">{title}</CategoryTitle>
+      <CategoryItemsContainer>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </CategoryItemsContainer>
+    </CategoryPageContainer>
+  );
 };
 const mapStateToProps = (state, ownProps) => {
-	return {
-		collection: selectCollection(ownProps.match.params.collectionId)(state)
-	};
+  return {
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
+  };
 };
 
 export default connect(mapStateToProps)(CollectionPage);
